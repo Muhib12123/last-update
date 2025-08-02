7861d24d2e45243f96e6d6ccb1e492ffbd143df7
Muhib12123 <ahmadestanbole@gmail.com>
2025-08-02 18:57:23 +0200

    Add files via upload

---
+++ fourth.py
@@ -0,0 +1,276 @@
+import sqlite3
+from flask import Flask, render_template
+import secrets
+
+
+def generate_secret_key():
+    return secrets.token_hex(16)
+
+
+
+
+
+db = sqlite3.connect("Muhib.db")
+
+cr = db.cursor()
+
+cr.execute("select * from user_skills")
+
+save = cr.fetchall()
+
+save2 = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Antigua & Deps",
+    "Argentina",
+    "Armenia",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia Herzegovina",
+    "Botswana",
+    "Brazil",
+    "Brunei",
+    "Bulgaria",
+    "Burkina",
+    "Burundi",
+    "Burma",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cape Verde",
+    "Central African Rep",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombia",
+    "Comoros",
+    "Congo",
+    "Congo {Democratic Rep}",
+    "Costa Rica",
+    "Croatia",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "East Timor",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Ethiopia",
+    "Fiji",
+    "Finland",
+    "France",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Greece",
+    "Grenada",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland [Republic]",
+    "Israel",
+    "Italy",
+    "Ivory Coast",
+    "Jamaica",
+    "Japan",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Korea North",
+    "Korea South",
+    "Kosovo",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macedonia",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Micronesia",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Qatar",
+    "Romania",
+    "Russian Federation",
+    "Rwanda",
+    "St Kitts & Nevis",
+    "St Lucia",
+    "Saint Vincent & the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome & Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Suriname",
+    "Swaziland",
+    "Sweden",
+    "Switzerland",
+    "Syria",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Togo",
+    "Tonga",
+    "Trinidad & Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Vatican City",
+    "Venezuela",
+    "Vietnam",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+]
+
+
+skills = Flask(__name__)
+skills.secret_key = generate_secret_key()
+
+@skills.route("/")
+def now():
+    return render_template(
+        "mypage.html", pagetitle="Muhib", self_css="my.css", data2=save2
+    )
+
+
+@skills.route("/about")
+def now2():
+    return render_template(
+        "about.html", pagetitle="About Muhib", self_css="about.css", data=save
+    )
+
+
+@skills.route("/skills")
+def now3():
+    return render_template(
+        "skills.html",
+        pagetitle="Family skills",
+        self_css="skills.css",
+        head="Family Skills",
+        des="You can see here our skills",
+        data=save,
+        head2="Skill Info",
+        des2="About skills",
+        head3="User Info",
+        des3="Details about users",
+    )
+
+
+
+@skills.route("/index")
+def now4():
+    return render_template(
+        "index.html",
+    )
+
+@skills.route("/phone")
+def now5():
+    return render_template(
+        "phone.html",
+    )
+
+@skills.route("/studying")
+def now6():
+    return render_template(
+        "studying.html"
+    )
+
+
+if __name__ == "__main__":
+    skills.run(debug=True, port=9000)
